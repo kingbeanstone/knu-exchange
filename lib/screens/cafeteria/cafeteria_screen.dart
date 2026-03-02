c827d9986560eb8fe8a48922dc6f9cc223c4e3ed
Hungry-f00l <kimtony598@gmail.com>
2026-03-02 16:04:46 +0900

    3/02 경대리아 정상적으로 보임

--- lib/screens/cafeteria/cafeteria_screen.dart
+++ lib/screens/cafeteria/cafeteria_screen.dart
@@ -24,7 +24,7 @@ class _CafeteriaScreenState extends State<CafeteriaScreen> {
     'welfare_bldg_cafeteria': '305(Welfare)',
     'information_center_cafeteria': '116(Info)',
     'engineering_bldg_cafeteria': '408(Engineer)',
-    'kyungdaria': '111(FastFood)',
+    'kyungdaria_cafeteria': '111(FastFood)',
   };
 
   @override
