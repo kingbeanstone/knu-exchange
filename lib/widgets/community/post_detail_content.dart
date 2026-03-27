df8c6043143c045eedfa8810534f2d0fec8fbee2
Hungry-f00l <kimtony598@gmail.com>
2026-03-27 12:28:18 +0900

    3/27 마크다운 수정

--- lib/widgets/community/post_detail_content.dart
+++ lib/widgets/community/post_detail_content.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart'; // ✅ 추가
 import '../common/full_screen_gallery.dart'; // ✅ 수정: 공통 갤러리 임포트
 
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:markdown/markdown.dart' as md;
 import '../../utils/link_utils.dart';
 
